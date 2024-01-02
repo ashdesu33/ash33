fe9b82aad3b499c58c6c982b4d86b722f1223df6
AshMa1022 <wma@risd.edu>
2024-01-01 22:08:38 -0500

    fix arrow

--- Scripts/load.js
+++ Scripts/load.js
@@ -1,6 +1,6 @@
 const navBar = document.querySelector('nav');
     const isDesktopView = window.matchMedia("(min-width: 768px)").matches;
-    const isHomePage = window.location.pathname === '/' || window.location.pathname === '/home.html';
+    const isHomePage = window.location.pathname === `https://github.com/AshMa1022/ash33/` || window.location.pathname === '/home.html';
 function loadNav(){
     
     console.log(isDesktopView);

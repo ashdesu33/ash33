00ed2349861668e1df972ce1e1cb8822b0c12d4d
AshMa1022 <wma@risd.edu>
2024-04-30 13:10:03 -0400

    try

--- Scripts/feature_nav.js
+++ Scripts/feature_nav.js
@@ -1,9 +1,16 @@
 let navContainer = "";
 const navbar = document.querySelector('.nav ul');
 function feature_nav(){
+    const currentPage = window.location.pathname;
     projects.forEach((project, index) => {
-        const projectPage = project.page.replace('Pages/', '');
-        navContainer += `<li><a href="${projectPage}">+ ${project.title}</a><i>(${project.category})</i></li>`
+        
+        if (currentPage.endsWith('home.html')) {
+            navContainer += `<li><a href="${project.page}">+ ${project.title}</a><i>(${project.category})</i></li>`
+        }
+        else{
+            const projectPage = project.page.replace('Pages/', '');
+            navContainer += `<li><a href="${projectPage}">+ ${project.title}</a><i>(${project.category})</i></li>`
+        }
     });
     navbar.innerHTML += navContainer;
   

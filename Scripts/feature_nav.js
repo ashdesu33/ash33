f323a4762839ae9de9bf70c029e2c538fff1cfbb
AshMa1022 <wma@risd.edu>
2024-11-06 18:17:51 -0500

    links

--- Scripts/feature_nav.js
+++ Scripts/feature_nav.js
@@ -4,12 +4,12 @@ function feature_nav(){
     const currentPage = window.location.pathname;
     projects.forEach((project, index) => {
         
-        if (currentPage.endsWith('index.html')) {
-            navContainer += `<li><hr><a href="${project.page}">+ ${project.title} </a><span>(${project.category})</span></li>`
+        if (currentPage.endsWith('index.html') || window.location.pathname === '/') {
+            navContainer += `<li onclick="goTo('${project.page}');"><hr><a href="${project.page}">+ ${project.title} </a><span>(${project.category})</span></li>`
         }
         else{
             const projectPage = project.page.replace('Pages/', '');
-            navContainer += `<li><hr><a href="${projectPage}">+ ${project.title} </a><span>(${project.category})</span></li>`
+            navContainer += `<li onclick="goTo('${project.page}');"><hr><a href="${projectPage}">+ ${project.title} </a><span>(${project.category})</span></li>`
         }
     });
     navbar.innerHTML += navContainer;
